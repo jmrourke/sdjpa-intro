42178c33dd5d3a3287c7027c90db4c0385599056
Jim Rourke <jmrourke@yahoo.com>
2023-09-25 15:14:56 -0400

    COMMIT JROURKE

--- src/main/java/guru/springframework/sdjpaintro/domain/Book.java
+++ src/main/java/guru/springframework/sdjpaintro/domain/Book.java
@@ -23,11 +23,13 @@ public class Book {
 
     }
 
+
     public Book(String title, String isbn, String publisher) {
         this.title = title;
         this.isbn = isbn;
         this.publisher = publisher;
     }
+
 
     public Long getId() {
         return id;
